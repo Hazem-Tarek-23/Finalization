ae51efb4c37bc8fc71abf534238aef54123e4993
Hazem-Tarek-23 <168303282+Hazem-Tarek-23@users.noreply.github.com>
2024-05-19 10:36:26 +0300

    Add files via upload

---
+++ Console with links.cpp
@@ -0,0 +1,27 @@
+#include "SearchEngine.h"
+using namespace std;
+
+Student* SearchEngine::search_by_id(const string& id) {
+    for (auto& student : data) {
+        if (student.student_id == id) {
+            return &student;
+        }
+    }
+    return nullptr;
+}
+
+Student* SearchEngine::search_by_name(const string& name) {
+    for (auto& student : data) {
+        if (student.name == name) {
+            return &student;
+        }
+    }
+    return nullptr;
+}
+
+void SearchEngine::update_data(const vector<Student>& students) {
+    data = students;
+}
+
+// githublink: https://github.com/ahmedantar6/StudentManagmentSystem
+// githublink2: https://github.com/Hazem-Tarek-23/Finalization.git
